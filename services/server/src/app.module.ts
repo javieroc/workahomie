4f9e678c01d27bb2bd9fcd62277c9a520649922c
Javier Ocampo <ferroxido@gmail.com>
2023-01-29 15:22:36 -0300

    feat: add authentication, fix docker

--- services/server/src/app.module.ts
+++ services/server/src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HostsModule } from './hosts/hosts.module';
+import { AuthzModule } from './authz/authz.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { HostsModule } from './hosts/hosts.module';
     MongooseModule.forRoot(
       `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     ),
+    AuthzModule,
     HostsModule,
   ],
   controllers: [AppController],
--- services/server/src/hosts/hosts.controller.ts
+++ services/server/src/hosts/hosts.controller.ts
@@ -6,7 +6,9 @@ import {
   Put,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtGuard } from 'src/authz/jwt.guard';
 import { HostsService } from './hosts.service';
 import { CreateHostDto } from './dto/create-host.dto';
 import { UpdateHostDto } from './dto/update-host.dto';
@@ -15,6 +17,7 @@ import { UpdateHostDto } from './dto/update-host.dto';
 export class HostsController {
   constructor(private readonly hostsService: HostsService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
   create(@Body() createHostDto: CreateHostDto) {
     return this.hostsService.create(createHostDto);
@@ -30,11 +33,13 @@ export class HostsController {
     return this.hostsService.findOne(id);
   }
 
+  @UseGuards(JwtGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateHostDto: UpdateHostDto) {
     return this.hostsService.update(id, updateHostDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.hostsService.remove(id);
