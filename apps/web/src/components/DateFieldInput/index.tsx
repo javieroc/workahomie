47bc84887da490e191cd30b438f9fe737222a395
Javier Ocampo <ferroxido@gmail.com>
2024-06-20 16:54:38 +0100

    styles: datepicker styles

--- apps/web/src/components/DateFieldInput/index.tsx
+++ apps/web/src/components/DateFieldInput/index.tsx
@@ -7,12 +7,14 @@ import {
   FormErrorMessage,
   FormControlProps,
 } from '@chakra-ui/react';
-import DatePicker from 'react-datepicker';
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import { FieldValues, useController, UseControllerProps } from 'react-hook-form';
 import 'react-datepicker/dist/react-datepicker.css';
 import classes from './styles.module.css';
 
-type DateFieldInputProps<T extends FieldValues> = FormControlProps & UseControllerProps<T>;
+type DateFieldInputProps<T extends FieldValues> = FormControlProps &
+  UseControllerProps<T> &
+  Pick<ReactDatePickerProps, 'selectsStart' | 'startDate' | 'selectsEnd' | 'endDate' | 'minDate'>;
 
 const CustomInput = forwardRef<InputProps, 'input'>((props, ref) => (
   <Input ref={ref} {...props} width="100%" />
@@ -41,7 +43,11 @@ function DateFieldInput<T extends FieldValues>({
       )}
       <DatePicker
         wrapperClassName={classes.fullWidth}
-        className={classes.fullWidth}
+        className={classes.general}
+        calendarClassName={classes.calendar}
+        dayClassName={() => classes.calendarDay}
+        weekDayClassName={() => classes.calendarDay}
+        monthClassName={() => classes.calendarMonth}
         value={field.value}
         onChange={field.onChange}
         selected={field.value}
