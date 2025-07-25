f8e7dfdd3e41df200b432efc78f14cf4637f5525
Javier Ocampo <ferroxido@gmail.com>
2025-07-25 12:38:55 +0100

    chore: add axios interceptor to check for auth0 token

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,35 +1,31 @@
 import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { WithAuth0Token } from './api/WithAuth0Token';
 import { Auth0ProviderWithNavigate } from './auth0-provider-with-navigate';
 import { AuthenticationGuard } from './components';
 import { TryHosting, Home, Host, Hosts, HowItWorks } from './pages';
-
-const MyRoutes: FC = WithAuth0Token(() => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/hosts/*" element={<Hosts />} />
-      <Route path="/how-it-works" element={<HowItWorks />} />
-      <Route path="/try-hosting" element={<AuthenticationGuard component={TryHosting} />} />
-      <Route path="/host/*" element={<AuthenticationGuard component={Host} />} />
-      <Route
-        path="*"
-        element={
-          <main style={{ padding: '1rem' }}>
-            <p>Not found</p>
-          </main>
-        }
-      />
-    </Routes>
-  );
-});
+import { Auth0ProviderWithInterceptor } from './api/Auth0ProviderWithInterceptor';
 
 const App: FC = () => {
   return (
     <BrowserRouter basename="/workahomie">
       <Auth0ProviderWithNavigate>
-        <MyRoutes />
+        <Auth0ProviderWithInterceptor>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/hosts/*" element={<Hosts />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/try-hosting" element={<AuthenticationGuard component={TryHosting} />} />
+            <Route path="/host/*" element={<AuthenticationGuard component={Host} />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>Not found</p>
+                </main>
+              }
+            />
+          </Routes>
+        </Auth0ProviderWithInterceptor>
       </Auth0ProviderWithNavigate>
     </BrowserRouter>
   );
