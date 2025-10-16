b6da317b90342b756c8e1587eef096bfd937a079
Javier Ocampo <ferroxido@gmail.com>
2025-10-16 19:34:13 +0100

    styles: Update host details, replace Image gallery

--- apps/web/src/pages/Hosts/views/Details/index.tsx
+++ apps/web/src/pages/Hosts/views/Details/index.tsx
@@ -1,21 +1,17 @@
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
-import { Divider, Grid, GridItem, Stack, useMediaQuery } from '@chakra-ui/react';
-import { ImageCarousel } from 'src/components';
+import { Divider, Grid, GridItem, Stack } from '@chakra-ui/react';
 import { useHost } from '../../hooks';
-import { HostTitle } from './components/HostTitle';
-import { GridImageGallery } from './components/GridImageGallery';
-import { HostUser } from './components/HostUser';
 import { RequestForm } from './components/RequestForm';
+import { HostTitle } from './components/HostTitle';
+import { HostUser } from './components/HostUser';
 import { Reviews } from './components/Reviews';
+import { Gallery } from './components/Gallery';
 
 const Details: FC = () => {
   const { hostId } = useParams<{ hostId: string }>();
   const { data: host, isLoading } = useHost(hostId!);
 
-  const [isLargerThanMD] = useMediaQuery('(min-width: 62em)');
-
-  const displayGallery = isLargerThanMD && (host?.pictures?.length ?? 0) >= 5;
   return (
     <Stack
       pt={['20px', '20px', '46px']}
@@ -25,8 +21,7 @@ const Details: FC = () => {
       spacing={8}
     >
       <HostTitle host={host} isLoading={isLoading} />
-      {displayGallery && <GridImageGallery images={host?.pictures} isLoading={isLoading} />}
-      {!displayGallery && <ImageCarousel images={host?.pictures ?? []} />}
+      <Gallery images={host?.pictures} isLoading={isLoading} />
       <Grid
         templateColumns={[
           'repeat(auto-fit, 1fr)',
