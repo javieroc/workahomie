f266f9cfd0f51d00ad2a3cd40770a6f2806901bd
Javier Ocampo <ferroxido@gmail.com>
2025-10-26 18:04:32 +0000

    chore: add helmet middleware

--- services/server/src/main.ts
+++ services/server/src/main.ts
@@ -1,9 +1,11 @@
+import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(8000);
 }
